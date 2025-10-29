6140003c94bbf20593058b8f176f06ef98750698
bhoomika-rohra <K250784@nu.edu.pk>
2025-10-29 11:23:12 +0500

    Create task6.c

---
+++ task6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; i++) {
+        for(j = i; j < rows; j++) {
+            printf("  ");
+        }
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+
+        printf(" | ");
+
+        for(j = i; j < rows; j++) {
+            printf("  ");
+        }
+        for(j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+
+        printf(" | ");
+
+        for(j = i; j < rows; j++) {
+            printf("  ");
+        }
+        for(j = 1; j <= i; j++) {
+            printf("%c ", 'A' + j - 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
