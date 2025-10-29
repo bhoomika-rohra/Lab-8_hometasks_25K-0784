2b58cbbd966b9de4d0b07c3565f58855ce3876ab
bhoomika-rohra <K250784@nu.edu.pk>
2025-10-29 10:10:14 +0500

    Create task3.c

---
+++ task3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int image[4][4] = {
+        {1, 0, 1, 0},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 1, 1}};
+    int inverted[4][4];
+    int count = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (image[i][j] == 1){
+                count++;
+            }    
+            inverted[i][j] = 1 - image[i][j];
+        }
+    }
+    printf("Original\t\tInverted\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++){
+            printf("%d ", image[i][j]);
+        }    
+        printf("\t\t");
+        for (int j = 0; j < 4; j++){
+            printf("%d ", inverted[i][j]);
+        }    
+        printf("\n");
+    }
+    printf("\nNumber of white pixels: %d\n", count);
+    return 0;
+}
